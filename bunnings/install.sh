7afd3ad776c1f6e1e948662107f8cbc69cfeacf0
STan1 <104175046+stanbun@users.noreply.github.com>
2026-04-23 06:23:36 +0000

    added gh and gh-dash

--- bunnings/install.sh
+++ bunnings/install.sh
@@ -109,6 +109,20 @@ else
     rm /tmp/delta.deb
 fi
 
+# ── gh (Official Github CLI) ────────────────────────────────────────────────────
+# can use snap but this is recommended by Github
+# add gpg key
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update
+apt_install gh
+
+# ── gh-dash ───────────────────────────────────────────────────────────────────
+# gh-dash to use gh above via terminal app
+# https://www.gh-dash.dev/getting-started/usage/
+gh extension install dlvhdr/gh-dash
+
 # ── lazygit ───────────────────────────────────────────────────────────────────
 echo "==> Installing lazygit"
 if installed lazygit; then
