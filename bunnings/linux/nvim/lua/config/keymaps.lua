21396e131537beeb240f248f7be427f00e50876c
STan1 <104175046+stanbun@users.noreply.github.com>
2026-04-23 05:52:00 +0000

    enable block visual
    <C-v> usually intercepted by paste, and <C-q> problematic on Dvorak
    <leader>v unoccupied

--- bunnings/linux/nvim/lua/config/keymaps.lua
+++ bunnings/linux/nvim/lua/config/keymaps.lua
@@ -6,3 +6,6 @@ local map = vim.keymap.set
 -- Insert newline in normal mode without entering insert mode
 map("n", "nl", "o<Esc>", { desc = "Insert line below" })
 map("n", "NL", "O<Esc>", { desc = "Insert line above" })
+
+-- enable block visual with leader
+map("n", "<Leader>v", "<C-v>", { desc = "Enter visual block when <C-v> taken", noremap = true })
